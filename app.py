6d2cdfcc4cf02bff742ca270d1a111ff9a48c4b4
Kira <kirvader>
2019-06-03 10:24:58 +0300

    fc

---
+++ app.py
@@ -0,0 +1,27 @@
+from models import *
+from playhouse.shortcuts import model_to_dict
+import json, falcon
+
+class UserIdResource():
+     def on_get(self, req, resp, user_id):
+         try:
+             user = OrgUser.get(OrgUser.id == user_id)
+             resp.body = json.dumps(model_to_dict(user))
+         except OrgUser.DoesNotExist:
+             resp.status = falcon.HTTP_404
+
+
+class UserResource():
+     def on_get(self, req, resp):
+         users = OrgUser.select().order_by(OrgUser.id)
+         resp.body = json.dumps([model_to_dict(u) for u in users])
+
+
+api = falcon.API()
+
+users = UserResource()
+users_id = UserIdResource()
+
+api.add_route('/users/', users)
+api.add_route('/users/{user_id}', users_id)
+
---
+++ models.py
@@ -0,0 +1,27 @@
+from peewee import *
+import uuid
+
+psql_db = PostgresqlDatabase(
+    'kira1',
+    user='kira1',
+    password='27701757',
+    host='127.0.0.1')
+
+
+def init_tables():
+    psql_db.create_tables([OrgUser], safe=True)
+
+
+def generate_users(num_users):
+    for i in range(num_users):
+        user_name = str(uuid.uuid4())[0:8]
+        OrgUser(username=user_name).save()
+
+
+class BaseModel(Model):
+    class Meta:
+        database = psql_db
+
+
+class OrgUser(BaseModel):
+    username = CharField(unique=True)
